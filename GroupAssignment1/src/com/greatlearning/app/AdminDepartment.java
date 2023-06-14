3ce470c9e6df0b63ff492d816b593aa223b4b04b
tahreem1709 <130833334+tahreem1709@users.noreply.github.com>
2023-06-14 18:15:10 +0530

    Add files via upload

---
+++ GroupAssignment1/src/com/greatlearning/app/AdminDepartment.java
@@ -0,0 +1,14 @@
+package com.greatlearning.app;
+
+public class AdminDepartment extends SuperDepartment {
+	public String departmentName() {
+		return "Admin Department";
+		}
+	public String getTodaysWork() {
+		return "Complete your Documents submission";
+		}
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+		}
+
+}
---
+++ GroupAssignment1/src/com/greatlearning/app/HRDepartment.java
@@ -0,0 +1,17 @@
+package com.greatlearning.app;
+
+public class HRDepartment extends SuperDepartment{
+	public String departmentName() {
+		return "HR Department";
+		}
+	public String getTodaysWork() {
+		return "Fill todays timesheet and mark your attendence";
+		}
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+		}
+	public String doActivity() {
+		return "Team Lunch";}
+
+
+}

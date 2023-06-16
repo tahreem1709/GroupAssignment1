883398af18dbda17fec83d83cb4c1cc6f057c4eb
tahreem1709 <130833334+tahreem1709@users.noreply.github.com>
2023-06-16 20:58:10 +0530

    Add files via upload

--- GroupAssignment1/src/com/greatlearning/app/DriverClass.java
+++ GroupAssignment1/src/com/greatlearning/app/DriverClass.java
@@ -1,42 +1,69 @@
 package com.greatlearning.app;
 
+import java.util.Scanner;
+
+
+
 public class DriverClass {
 
 	public static void main(String[] args) {
+		
+		
+		Scanner in=new Scanner(System.in);
+		SuperDepartment s1=new SuperDepartment();
 		AdminDepartment a1=new AdminDepartment();
-		a1.departmentName();
-		System.out.println("Welcome to Admin Department");
-		a1.getTodaysWork();
-		System.out.println("Complete your Documents submission");
-		a1.getWorkDeadline();
-		System.out.println("Complete by EOD");
-		a1.isTodayAHoliday();
-		System.out.println("Today is not a Holiday");
-		
 		HRDepartment h1=new HRDepartment();
-		h1.departmentName();
-		System.out.println("Welcome to HR Department");
-		h1.doActivity();
-		System.out.println("Team Lunch");
-		h1.getTodaysWork();
-		System.out.println("Fill todays timesheet and mark your attendence");
-		h1.getWorkDeadline();
-		System.out.println("Complete by EOD");
-		h1.isTodayAHoliday();
-		System.out.println("Today is not a Holiday");
-		
 		TechDepartment t1=new TechDepartment();
-		t1.departmentName();
-		System.out.println("Welcome to Tech Department");
-		t1.getTodaysWork();
-		System.out.println("Complete coding of module 1");
-		t1.getTodaysWork();
-		System.out.println("Complete by EOD");
-		t1.getTechStackInformation();
-		System.out.println("Core Java");
-		t1.isTodayAHoliday();
-		System.out.println("Today is not a Holiday");
 		
+		System.out.println("1.Admin Department");
+		System.out.println("2.HR Department");
+		System.out.println("3.Tech Department");
+		
+		System.out.println("Enter Your Department Name!");
+		
+		int choice=in.nextInt();
+		
+		
+		switch(choice)
+		{
+		case 1:	
+			System.out.println(a1.departmentName());
+		
+		    System.out.println(a1.getTodaysWork());
+		
+		    System.out.println(a1.getWorkDeadline());
+		
+		    System.out.println(a1.isTodayAHoliday());
+		    break;
+		    
+		case 2:
+			System.out.println(h1.departmentName());
+			
+		    System.out.println(h1.doActivity());
+		    
+		    System.out.println(h1.getTodaysWork());
+		
+		    System.out.println(a1.getWorkDeadline());
+		
+		    System.out.println(a1.isTodayAHoliday());
+		    break;
+		    
+		case 3:
+			System.out.println(t1.departmentName());
+			
+		    System.out.println(t1.getTodaysWork());
+		
+		    System.out.println(t1.getWorkDeadline());
+		    
+		    System.out.println(t1.getTechStackInformation());
+		    
+		    System.out.println(a1.isTodayAHoliday());
+		    break;
+		    
+		    default: 
+		    	System.out.println("Please enter a valid department!");
+		    
+		}
 		
 		
 
